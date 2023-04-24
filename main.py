4fc1fbe8f99255ee90b19ec91a2a064074f7444e
EshitaMehta <itiseshita@gmail.com>
2023-04-24 13:09:56 +0530

    commit 3

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ def index():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    uvicorn.run("main:app", port=8008, reload=True)
+    uvicorn.run("main:app", port=8000, reload=True)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
