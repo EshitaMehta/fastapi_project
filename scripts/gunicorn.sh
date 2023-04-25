bd61562b29dd28834d7cf0a950e96540f3ee8d93
EshitaMehta <itiseshita@gmail.com>
2023-04-25 18:05:03 +0530

    change gunicorn

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #sudo cp /home/ubuntu/cicd/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
-sudo cp /home/ubuntu/fastapi_project/gunicorn.service  /etc/systemd/system/fastapi_project.service
+sudo cp /home/ubuntu/fastapi_project/gunicorn/gunicorn.service  /etc/systemd/system/fastapi_project.service
 
 sudo systemctl start fastapi_project
 sudo systemctl enable fastapi_project
