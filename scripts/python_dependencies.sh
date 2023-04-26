d8469c716ee094b91c789cb63d43ccf081a870f3
EshitaMehta <itiseshita@gmail.com>
2023-04-26 19:27:52 +0530

    change env

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -2,3 +2,6 @@
 python3 -m venv /var/www/env
 source /var/www/env/bin/activate
 pip install -r /var/www/fastapi_project/requirements.txt
+pip install gunicorn
+pip install fastapi[all]
+pip install wheel
