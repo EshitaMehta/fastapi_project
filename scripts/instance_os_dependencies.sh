5546f4fb0707314b542cc61569bae142911e1bee
EshitaMehta <itiseshita@gmail.com>
2023-04-27 16:34:24 +0530

    env changes

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -3,6 +3,6 @@
 sudo apt install -y python3-pip
 sudo apt install -y nginx
 # sudo apt install -y virtualenv
-# sudo apt install -y python3.8-venv
-sudo apt install python3-venv
+sudo apt install -y python3.8-venv
+#sudo apt install python3-venv
 sudo chown -R $USER /var/www
